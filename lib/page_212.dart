f4bea9600d6065cb6626259a501f341900ba6d3b
choiJunHo <wnsghqwe@gmail.com>
2021-04-06 14:41:39 +0900

    리팩토링 : ImageText Widget 생성

--- lib/page_212.dart
+++ lib/page_212.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter_appds/image_text.dart';
 
 void main() => runApp(MyApp());
 
@@ -77,7 +78,7 @@ class _Page1State extends State<Page1> {
   final dummyItems = [
     'https://newsimg.sedaily.com/2021/03/02/22JNI0BLJ4_1.jpg',
     'https://pbs.twimg.com/media/ELbI9xNUwAEA6ZA.jpg',
-    'http://image.newsis.com/2021/03/05/NISI20210305_0000701728_web.jpg',
+    'https://i.ytimg.com/vi/V343XU9mIU8/maxresdefault.jpg',
     'https://file.mk.co.kr/meet/neds/2019/09/image_readtop_2019_733188_15686182793901764.png'
   ];
 
@@ -95,59 +96,16 @@ class _Page1State extends State<Page1> {
   Widget _biuildTop() {
     return Column(
       children: [
+        SizedBox(
+          height: 20,
+        ),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            GestureDetector(
-              onTap: () {
-                print('클릭');
-                _showDialog();
-              },
-              child: Column(
-                children: <Widget>[
-                  Image.network(
-                    'https://img.icons8.com/ios/452/taxi.png',
-                    width: 80,
-                    height: 80,
-                    fit: BoxFit.cover,
-                  ),
-                  Text('택시')
-                ],
-              ),
-            ),
-            Column(
-              children: <Widget>[
-                Image.network(
-                  'https://www.clipartmax.com/png/middle/191-1914417_roaming-around-rome-taxi-taxi-icon-png-blue.png',
-                  width: 80,
-                  height: 80,
-                  fit: BoxFit.cover,
-                ),
-                Text('블루 택시')
-              ],
-            ),
-            Column(
-              children: <Widget>[
-                Image.network(
-                  'https://img.icons8.com/ios/452/taxi.png',
-                  width: 80,
-                  height: 80,
-                  fit: BoxFit.cover,
-                ),
-                Text('택시')
-              ],
-            ),
-            Column(
-              children: <Widget>[
-                Image.network(
-                  'https://img.icons8.com/ios/452/taxi.png',
-                  width: 80,
-                  height: 80,
-                  fit: BoxFit.cover,
-                ),
-                Text('택시')
-              ],
-            ),
+            ImageText('https://img.icons8.com/ios/452/taxi.png', '택시'),
+            ImageText('https://img.icons8.com/ios/452/bus.png', '버스'),
+            ImageText('https://i.pinimg.com/originals/23/ef/33/23ef33c7af0907557d07c6c9d10385e8.png', '기차'),
+            ImageText('https://img.icons8.com/ios/452/taxi.png', '비행기'),
           ],
         ),
         SizedBox(
@@ -157,39 +115,9 @@ class _Page1State extends State<Page1> {
           // Column 들을 Row 로 감싼다.
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Column(
-              children: <Widget>[
-                Image.network(
-                  'https://img.icons8.com/ios/452/taxi.png',
-                  width: 80,
-                  height: 80,
-                  fit: BoxFit.cover,
-                ),
-                Text('택시')
-              ],
-            ),
-            Column(
-              children: <Widget>[
-                Image.network(
-                  'https://img.icons8.com/ios/452/taxi.png',
-                  width: 80,
-                  height: 80,
-                  fit: BoxFit.cover,
-                ),
-                Text('택시')
-              ],
-            ),
-            Column(
-              children: <Widget>[
-                Image.network(
-                  'https://img.icons8.com/ios/452/taxi.png',
-                  width: 80,
-                  height: 80,
-                  fit: BoxFit.cover,
-                ),
-                Text('택시')
-              ],
-            ),
+            ImageText('https://img.icons8.com/ios/452/taxi.png', '택시'),
+            ImageText('https://img.icons8.com/ios/452/taxi.png', '택시'),
+            ImageText('https://img.icons8.com/ios/452/taxi.png', '택시'),
             SizedBox(
               width: 80,
               height: 80,
@@ -236,8 +164,8 @@ class _Page1State extends State<Page1> {
     });
 
     return ListView(
-      physics: NeverScrollableScrollPhysics(),
-      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(), // 이 리스트의 스크롤을 금지
+      shrinkWrap: true, // 이 리스트가 다른 스크롤 객체 안에 있다면 true로 설정 해야 함
       children: items,
     );
   }
